8ad470edc328c2e4b15d40baf9876d07f754ac4e
Apple1308 <ruslonxudayberganov2@gmail.com>
2023-02-25 21:40:34 +0500

    -firstCommit

---
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Ruslon Xudayberganov on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
